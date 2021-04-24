2af8afbbfb8b195ecca5042ff0359df7aa4ed0da
Vladimir Stepanenko <vovac12@gmail.com>
2021-04-24 22:07:18 +0300

    gh-1 Create app template

---
+++ android/app/src/main/kotlin/com/example/yad/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("YAD User app"),
+        ),
+        body: Center(
+          child: Text("Template"),
+        ),
+      ),
+    );
+  }
+}
