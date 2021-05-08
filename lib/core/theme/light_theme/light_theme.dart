435424c016c7776b591403c82dd3036c32ddaf24
k.polyakova <KsenyaPolyakovaaa@mail.ru>
2021-05-08 11:27:11 +0300

    gh-23 corrected theme file (added import restaurant card theme)

--- lib/core/theme/light_theme/light_theme.dart
+++ lib/core/theme/light_theme/light_theme.dart
@@ -9,6 +9,7 @@ part 'light_app_bar_theme.dart';
 part 'light_input_theme.dart';
 part 'light_data_input_card_theme.dart';
 part 'light_registration_card_theme.dart';
+part 'light_restaurant_card_theme.dart';
 
 const Color _yellow = Color.fromRGBO(251, 187, 0, 1);
 const Color _orange = Color.fromRGBO(248, 113, 34, 1);
@@ -29,6 +30,7 @@ class LightTheme implements ITheme {
       LightDataInputCardTheme();
   static LightRegistrationCardTheme _registrationCardTheme =
       LightRegistrationCardTheme();
+  static LightRestaurantCardTheme _restaurantCardTheme = LightRestaurantCardTheme();
 
   @override
   IProfileTheme get profileTheme => _profileTheme;
