a7273da911f6400d6936a3ad4b796c757b620c8c
k.polyakova <KsenyaPolyakovaaa@mail.ru>
2021-05-08 17:42:21 +0300

    gh-21 corrected theme for restaurant card

--- lib/widgets/restaurant_card.dart
+++ lib/widgets/restaurant_card.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:yad/core/theme/light_theme/light_theme.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:yad/core/theme/i_theme/i_theme.dart';
 import 'package:yad/widgets/dish_card.dart';
 
 class RestaurantCard extends StatelessWidget {
   final ImageProvider _photo;
   final String header;
-  final LightTheme _lightTheme;
 
   RestaurantCard({ImageProvider? photo, required this.header})
-      : _photo = photo ?? AssetImage('assets/restaurant_0.jpg'),
-        _lightTheme = LightTheme();
+      : _photo = photo ?? AssetImage('assets/restaurant_0.jpg');
 
   @override
   Widget build(BuildContext context) {
+    final theme = context.read<ITheme>();
+
     return Container(
-        height: _lightTheme.restaurantCardTheme.cardHeight,
+        height: theme.restaurantCardTheme.cardHeight,
         child: Card(
-            color: _lightTheme.restaurantCardTheme.colorCard,
-            shape: _lightTheme.restaurantCardTheme.shapeCard,
+            color: theme.restaurantCardTheme.colorCard,
+            shape: theme.restaurantCardTheme.shapeCard,
             child: ListTile(
               title: Container(
-                width: _lightTheme.restaurantCardTheme.photoWidth,
-                height: _lightTheme.restaurantCardTheme.photoHeight,
+                width: theme.restaurantCardTheme.photoWidth,
+                height: theme.restaurantCardTheme.photoHeight,
                 decoration: BoxDecoration(
                   shape: BoxShape.rectangle,
-                  borderRadius:
-                      _lightTheme.restaurantCardTheme.widgetBorderRadius,
+                  borderRadius: theme.restaurantCardTheme.widgetBorderRadius,
                   image: DecorationImage(
                     image: _photo,
                     fit: BoxFit.cover,
@@ -34,7 +34,7 @@ class RestaurantCard extends StatelessWidget {
               ),
               subtitle: Text(
                 this.header,
-                style: _lightTheme.restaurantCardTheme.subtitleTextStyle,
+                style: theme.restaurantCardTheme.subtitleTextStyle,
               ),
               onTap: () {
                 Navigator.push(
